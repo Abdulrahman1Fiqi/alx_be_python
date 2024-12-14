5b2cc1579173ff8061722e1d137de10169825ee1
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-14 17:54:17 +0200

    make changes

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,17 +4,17 @@ y = input("Is it time-bound? (yes/no): ")
 match x:
     case "high":
         if y=="yes":
-            print("Reminder: 'Finish project report' is a high priority task that requires immediate attention today!")
-        else:
-            print("Note: 'Read a book' is a low priority task. Consider completing it when you have free time.")
+            print(f"Reminder: {task} is a {x} priority task that requires immediate attention today!")
+        elif y=="no":
+            print(f"Note: {task} is a {x} priority task. Consider completing it when you have free time.")
     case "medium":
         if y=="yes":
-            print("Reminder: 'Finish project report' is a medium priority task that requires immediate attention !")
-        else:
-            print("Note: 'Read a book' is a low priority task. Consider completing it when you have free time.")
+            print(f"Reminder: {task} is a {x} priority task that requires immediate attention today!")
+        elif y=="no":
+            print(f"Note: {task} is a {x} priority task. Consider completing it when you have free time.")
     case "low":
         if y=="yes":
-            print("Reminder: 'Finish project report' is a low priority task that requires immediate attention !")
-        else:
-            print("Note: 'Read a book' is a low priority task. Consider completing it when you have free time.")
+            print(f"Reminder: {task} is a {x} priority task that requires immediate attention today!")
+        elif y=="no":
+            print(f"Note: {task} is a {x} priority task. Consider completing it when you have free time.")
 
