672afe65636fe0d697ea968d85430574453303ab
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-20 13:22:52 +0200

    make changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,8 +7,9 @@ def perform_operation(num1, num2, operation):
         case 'multiply':
             result = num1 * num2
         case 'divide':
-            if num2 == 0:
-                print("error")
-            else:
+            if num2 != 0:
                 result = num1 / num2
+            elif num2 == 0:
+                print("error")
+                
     return result
