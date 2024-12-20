d71fc2916b459f9ec5577fe34ee43e0630cbb11e
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-20 13:33:11 +0200

    make changes

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,30 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item = input("Enter the name of the new item: ")
+            shopping_list.append(item)
+        elif choice == '2':
+            item = input("Enter the name of the item you want to delete: ")
+            shopping_list.pop(item)
+        elif choice == '3':
+            for x in shopping_list:
+                print(x)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
