656bf6fd4bfdf60bafd5b3524a70c85267875bef
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-20 20:01:46 +0200

    make changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,9 +9,8 @@ class BankAccount:
     def withdraw(self,amount):
         if self.account_balance>=amount:
             self.account_balance -=amount
-            #print(f"Withdrew: ${amount}")
         else:
-            print("Insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
