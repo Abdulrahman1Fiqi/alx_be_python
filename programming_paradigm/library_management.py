d923badadc8b4669c7e9492bb04d1a35e6be7ec8
Abdulrahman Al_Fiqi <abdulrahmanhmh47@gmail.com>
2024-12-21 07:17:24 +0200

    make changes

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -22,13 +22,13 @@ class Book:
 
 class Library:
     def __init__(self):
-        self.__books = []  # Private list to store Book instances
+        self._books = []  # Private list to store Book instances
 
-    def add_Book(self, book):
+    def add_book(self, book):
         if isinstance(book, Book):
             self.__books.append(book)
 
-    def check_out_Book(self, title):
+    def check_out_book(self, title):
         for book in self.__books:
             if book.title == title and book.is_available():
                 if book.check_out():
@@ -36,7 +36,7 @@ class Library:
                     return
         print(f"The book '{title}' is not available for checkout.")
 
-    def return_Book(self, title):
+    def return_book(self, title):
         for book in self.__books:
             if book.title == title and not book.is_available():
                 if book.return_book():
@@ -44,7 +44,7 @@ class Library:
                     return
         print(f"The book '{title}' is not checked out.")
 
-    def listavailablebooks(self):
+    def list_available_books(self):
         available_books = [book.title for book in self.__books if book.is_available()]
         if available_books:
             print("Available books:")
